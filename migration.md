d1bb9b50fdfcabe805933b88f7470d52aa6d54c7
Diego Almeida <dyhalmeida@gmail.com>
2020-10-13 10:37:51 -0300

    Configuração da migration

---
+++ migration.md
@@ -0,0 +1,15 @@
+### Comando para gerar uma "migration"
+
+```shell
+yarn typeorm migration:create -n *migration_name
+
+# Ex:
+yarn typeorm migration:create -n create_orphanages
+```
+
+### Comando para executar as "migrations"
+
+```shell
+yarn typeorm migration:run
+```
+
---
+++ src/database/migrations/1602594947838-create_orphanages.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createOrphanages1602594947838 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true, // não aceita negativo
+            isPrimary: true,
+            isGenerated: true, // coluna gerada automaticamente
+            generationStrategy: 'increment', // auto incrementada
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'bolean',
+            default: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
+}
