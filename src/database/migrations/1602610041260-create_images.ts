6e6a873fcba2c35db41b416c7ea6f7a4a47499a3
Diego Almeida <dyhalmeida@gmail.com>
2020-10-13 15:41:02 -0300

    Criação de migration para tabela de imagens

---
+++ src/database/migrations/1602610041260-create_images.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createImages1602610041260 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'images',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true, // não aceita negativo
+            isPrimary: true,
+            isGenerated: true, // coluna gerada automaticamente
+            generationStrategy: 'increment', // auto incrementada
+          },
+          {
+            name: 'path',
+            type: 'varchar',
+          },
+          {
+            name: 'orphanage_id',
+            type: 'integer',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'ImageOrphanage',
+            columnNames: ['orphanage_id'],
+            referencedTableName: 'orphanages',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('images');
+  }
+}
