f9be903d28f37f73ff72e19aa99f7f28ddf1f874
Diego Almeida <dyhalmeida@gmail.com>
2020-10-13 16:59:28 -0300

    Adicionado cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
@@ -6,6 +7,7 @@ import { join } from 'path';
 import errorHandler from './errors/handler';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
